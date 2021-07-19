7c3d14a3410fedfde74f1b6056a8d8b3497f5c10
vanya2233 <84853715+vanya2233@users.noreply.github.com>
2021-07-19 12:33:50 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/about.css">
+    <title>National Bank</title>
+  </head>
+
+  
+  <body background="img/bg1.jpg">
+  <?php
+  include 'navbar.php';
+  ?>
+
+<div class="abouta">
+<p><h6>
+National Bank is a leading private sector bank in India. The Bank’s consolidated total assets stood at Rs. 30.69 trillion at September 30, 2020.  National Bank currently has a network of 4,789 branches and 19,201 ATMs across India.
+</h6></p><hr>
+<p>
+<b>History</b><br>
+National Bank was originally promoted in 1994 by ICICI Limited, an Indian financial institution, and was its wholly-owned subsidiary.
+</p><hr>
+<p>
+<b>National Group Companies</b><br>
+National Bank offers a wide range of banking products and financial services to corporate and retail customers through a variety of delivery channels and through its group companies.
+</p><hr>
+<p>
+<b>Board of Directors</b><br>
+National Bank's Board members include eminent individuals with a wealth of experience in international business, management consulting, banking and financial services.
+</p><hr>
+<p>
+<b>Investor Relations</b><br>
+All the latest, in-depth information about National Bank's financial performance and business initiatives.
+</p><hr>
+<p>
+<b>Career Opportunities</b><br>
+Explore diverse openings with National Bank.
+</p><hr>
+<p>
+<b>Awards</b><br>
+Time and again our innovative banking services has been recognized and rewarded world over.
+</p><hr>
+<p>
+<b>News Room</b><br>
+Catch up with the Pandera Bank's latest business and social initiatives, as well as innovative product launches.
+</p><hr>
+
+ 
+<p>
+<b>Corporate Social Responsibility</b><br>
+National Bank is deeply engaged in human and economic development at the national level. The Bank works closely with National Foundation across diverse sectors and programs.
+</p><hr>
+ 
+<p>
+<b>Notice Board</b><br>
+Catch up with National Bank's latest communication related to Acknowledgements, information on regulatory notices, banking ombudsman schemes and others.
+</p><hr>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  
+      
+      <footer class="text-center mt-5 py-2">
+        <p> Made by <b>Vanya Yadav</b> <br>Copyright &copy 2021 <br> Sparks Foundation</p>
+      </footer>
+     
+  </body>
+</html>
